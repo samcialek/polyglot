0d1f4ebc1792914c150fc3642ab8bdaaaf6ced5f
Sam Cialek <scialek3@gatech.edu>
2026-08-16 14:18:03 +0000

    Add promise.all from scratch in JavaScript

---
+++ snippets/2026/08-16-javascript.js
@@ -0,0 +1,26 @@
+/**
+ * Re-implementation of Promise.all for learning purposes.
+ */
+function promiseAll(promises) {
+  return new Promise((resolve, reject) => {
+    const results = new Array(promises.length);
+    let remaining = promises.length;
+    if (remaining === 0) return resolve(results);
+    promises.forEach((p, i) => {
+      Promise.resolve(p).then(
+        (val) => {
+          results[i] = val;
+          if (--remaining === 0) resolve(results);
+        },
+        reject
+      );
+    });
+  });
+}
+
+// Demo
+promiseAll([
+  Promise.resolve(1),
+  new Promise((r) => setTimeout(() => r(2), 50)),
+  Promise.resolve(3),
+]).then(console.log); // [1, 2, 3]
